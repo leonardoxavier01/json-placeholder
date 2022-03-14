6cc09e758640b943b737bcce302347f9241ece61
unknown <leonardoxavier092@gmail.com>
2022-03-13 22:58:52 -0300

    test: fetch api json-placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,38 @@ import './App.css'
 
 class App extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        title: 'O titule exemplo 1',
-        body: 'O texto exemplo 1',
-      }
-    ]
+    posts: []
+  };
+
+  componentDidMount() {
+    this.loadPosts();
   }
 
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
 
-render() {
-  const { posts } = this.state;
-  return (
-    <div>
-      {posts.map(post => <h1>{post.title}</h1>)}
-    </div>
-  );
-}
+    const [posts] = await Promise.all([postsResponse]);
+
+    const postsJson = await posts.json();
+
+    this.setState({ posts: postsJson });
+
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 };
 
 

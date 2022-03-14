1c8447e1fe97f8f1e201796b008df0c0bb021a71
unknown <leonardoxavier092@gmail.com>
2022-03-14 19:15:48 -0300

    perf: creating components - test: first props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import react from 'react';
 import { Component } from 'react'
 import './App.css'
+import { PostCard } from './components/PostCard';
 
 class App extends Component {
   state = {
@@ -36,13 +36,8 @@ class App extends Component {
       <section className='container'>
         <div className='posts'>
           {posts.map(post => (
-            <div className='post'>
-              <img src={post.cover} alt={post.title}/>
-              <div key={post.id} className='post-content'>
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-              </div>
-            </div>
+            <PostCard
+            post={post}/>
           ))}
         </div>
       </section>
